4e48f0840e7da95690935e2501ceeb4efaa33d60
Paulo Freitas <paulo.freitas.nt@gmail.com>
2023-02-08 13:40:23 -0300

    Adicionando método para remover pessoa

--- index.js
+++ index.js
@@ -7,8 +7,14 @@ const client = new cassandra.Client({
 });
 
 async function addPessoa(pessoa){
+    //Pode ser utilizado também para a atualização
     await client.execute('INSERT INTO pessoa (id, nome, email) VALUES (?,?,?)',[pessoa.id, pessoa.nome, pessoa.email],{ prepare: true });
     console.log('Inserido');
+}
+
+async function deletarPessoa(id){
+    await client.execute('DELETE FROM pessoa WHERE id = ?', [id], {prepare: true});
+    console.log('Removido');
 }
 
 const pessoa = {
@@ -17,4 +23,5 @@ const pessoa = {
     email: 'maria@gmail.com'
 }
 
-addPessoa(pessoa);
+// addPessoa(pessoa);
+deletarPessoa(2);
